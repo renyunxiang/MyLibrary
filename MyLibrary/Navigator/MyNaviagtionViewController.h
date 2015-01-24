281472d0d34ef76179ee37db1204146ef17d1ad5
jiangyi <nero@nerodeMacBook-Pro.local>
2015-01-24 16:48:42 +0800

    新增 通过手势移除控制器
    将导航控制器 切换为自定义的控制器即可

---
+++ MyLibrary/Navigator/MyNaviagtionViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyNaviagtionViewController.h
+//  MyLibrary
+//
+//  Created by nero on 15/1/24.
+//  Copyright (c) 2015年 nero. All rights reserved.
+//   将导航控制器切换为自己的
+//  即可实现在任意位置可通过手势移除控制器
+
+#import <UIKit/UIKit.h>
+
+@interface MyNaviagtionViewController : UINavigationController
+
+@end
